66b7c2260b261669a1c4008f53f725ea6dae6c60
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-07-09 23:38:17 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+//Q:02 personal message:store a person`s name in a variable, and print a message to that person,your message should be simple,such as, "hello Eric,would you like to learn some python today
+var hi = "Saba Nizam";
+console.log("Hello ".concat(hi, ",would you like to learn some python today"));
---
+++ main.ts
@@ -0,0 +1,7 @@
+//Q:02 personal message:store a person`s name in a variable, and print a message to that person,your message should be simple,such as, "hello Eric,would you like to learn some python today
+
+
+let hi = `Saba Nizam`
+
+console.log(`Hello ${hi},would you like to learn some python today`)
+
